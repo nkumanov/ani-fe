17d22c06ea01f08fd3f5a0437b1a2d00f5e0c497
nkumanov <kumanov9696@gmail.com>
2025-04-23 08:51:06 +0300

    new table design

--- src/components/Guests/Guests.tsx
+++ src/components/Guests/Guests.tsx
@@ -13,23 +13,81 @@ enum Meal {
 }
 function Guests() {
   const { data, isLoading, error } = useGetAllGuestsQuery();
+  const [filter, setFilter] = useState<"all" | Attend>("all");
   if (isLoading) return <p>Loading...</p>;
+  const showGuests = (filter: "all" | Attend) => {
+    switch (filter) {
+      case "all": {
+        return data?.map((user, index) => (
+          <tr key={user._id}>
+            <td>{index + 1}</td>
+            <td>{user.name}</td>
+            <td>{user.attend ? UserAttend[user.attend] : "Няма информация"}</td>
+            <td>{user.meal ? Meal[user.meal] : ""}</td>
+            <td>{user.alergy ? user.alergy : ""}</td>
+          </tr>
+        ));
+      }
+      case Attend.Comming: {
+        return data
+          ?.filter((guest) => guest.attend === Attend.Comming)
+          .map((user, index) => (
+            <tr key={user._id}>
+              <td>{index + 1}</td>
+              <td>{user.name}</td>
+              <td>
+                {user.attend ? UserAttend[user.attend] : "Няма информация"}
+              </td>
+              <td>{user.meal ? Meal[user.meal] : ""}</td>
+              <td>{user.alergy ? user.alergy : ""}</td>
+            </tr>
+          ));
+      }
+      case Attend.NotComming: {
+        return data
+          ?.filter((guest) => guest.attend === Attend.NotComming)
+          .map((user, index) => (
+            <tr key={user._id}>
+              <td>{index + 1}</td>
+              <td>{user.name}</td>
+              <td>
+                {user.attend ? UserAttend[user.attend] : "Няма информация"}
+              </td>
+              <td>{user.meal ? Meal[user.meal] : ""}</td>
+              <td>{user.alergy ? user.alergy : ""}</td>
+            </tr>
+          ));
+      }
+    }
+  };
   return (
     <section className={styles.wrapper}>
       <h1>List of all guests</h1>
       {isLoading && <p>Loading...</p>}
       {data && (
-        <p>
+        <p className={styles.numberOfGuests}>
           Брой присъстващи гости:{" "}
           {data.filter((guest) => guest.attend === Attend.Comming).length}
         </p>
       )}
       {data && (
-        <p>
+        <p className={styles.numberOfGuests}>
           Брой НЕ присъстващи гости:{" "}
           {data.filter((guest) => guest.attend === Attend.NotComming).length}
         </p>
       )}
+      <div className={styles.formElementSelect}>
+        <label htmlFor="">Присъствие:</label>
+        <select
+          name="guestCount"
+          id=""
+          onChange={(e) => setFilter(e.currentTarget.value as any)}
+        >
+          <option value="all">Всички</option>
+          <option value={Attend.Comming}>Присъстващи</option>
+          <option value={Attend.NotComming}>Неприсъстващи</option>
+        </select>
+      </div>
       {error && <p>Възникна някаква грешка. Моля опитайте по-късно.</p>}
       {data && (
         <table>
@@ -42,20 +100,7 @@ function Guests() {
               <th>Алергия</th>
             </tr>
           </thead>
-          <tbody>
-            {data &&
-              data?.map((user, index) => (
-                <tr key={user._id}>
-                  <td>{index + 1}</td>
-                  <td>{user.name}</td>
-                  <td>
-                    {user.attend ? UserAttend[user.attend] : "Няма информация"}
-                  </td>
-                  <td>{user.meal ? Meal[user.meal] : ""}</td>
-                  <td>{user.alergy ? user.alergy : ""}</td>
-                </tr>
-              ))}
-          </tbody>
+          <tbody>{data && showGuests(filter)}</tbody>
         </table>
       )}
     </section>
