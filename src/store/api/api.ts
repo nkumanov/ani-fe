98894dcc4757811591c69f2c470b1566d9b73571
nkumanov <kumanov9696@gmail.com>
2025-05-04 09:53:48 +0300

    new endpoint

--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -3,8 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000",
-    // baseUrl: "https://wedding-be-ny2a.onrender.com",
+    baseUrl: "https://wedding-be-ny2a.onrender.com",
   }),
   endpoints: () => ({}),
 });
